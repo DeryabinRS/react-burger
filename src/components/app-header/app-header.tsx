5766f027a98c0fa6473611605a50748742e6f1e0
Дерябин Роман Сергеевич <deryabinrs@tpu.ru>
2022-03-18 15:58:19 +0700

    add header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './app-header.module.css'
 
 const AppHeader = () => {
     //const useState['primary']
     return (
         <header className={styles.header}>
-            <a href="#">
+            <a href="#" className={styles.link}>
                 <BurgerIcon type="primary" /> Конструктор
             </a>
         </header>
