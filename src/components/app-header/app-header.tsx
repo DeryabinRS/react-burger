807271a02934767c7e1054aa06ec1c7cc1374bd2
Deryabin Roman <deryabinrs@gmail.com>
2022-04-12 00:03:28 +0700

    scroll tab

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './app-header.module.css'
 
--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import { FC, useState, useEffect, useRef } from 'react'
 import styles from './burger-ingredients.module.css'
 import IngredientsCard from '../ingredients-card/ingredients-card'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
@@ -7,13 +7,32 @@ import { fetchIngredients } from '../../services/store/actions/action-ingredient
 
 const BurgerIngredients: FC = (): JSX.Element => {
     const dispatch = useAppDispatch()
-    const [current, setCurrent] = React.useState('one')
+    const [current, setCurrent] = useState('one')
+
+    const refBlock = useRef<HTMLDivElement>(null)
+    const refTitle1 = useRef<HTMLDivElement>(null)
+    const refTitle2 = useRef<HTMLDivElement>(null)
+    const refTitle3 = useRef<HTMLDivElement>(null)
 
     const {ingredients} = useAppSelector(state => state.ingredientsSlice)
 
     useEffect(() => {
         dispatch(fetchIngredients())
     },[])
+
+    const handleScroll = () => {
+        const scrollY = refBlock?.current?.scrollTop || 0
+        const refBlockTop = refBlock?.current?.offsetTop || 0;
+        const sections = [refTitle1, refTitle2, refTitle3];
+        let current:any = '';
+        sections.forEach((section, i) => {
+            const sectionTop = Math.abs(refBlockTop - (section.current?.offsetTop || 0));
+            if(scrollY > sectionTop){
+                current = section.current?.getAttribute('id')
+                setCurrent(current)
+            }
+        });
+    }
 
     return (
         <div className={styles.wrapper}>
@@ -29,18 +48,24 @@ const BurgerIngredients: FC = (): JSX.Element => {
                     Начинки
                 </Tab>
             </div>
-            <div className={`${styles.wrapper_indredients} mt-10 scroll`}>
-                <h2 className={styles.title}>Булки</h2>
-                <div className={`${styles.ingredient_list} mb-10`}>
-                    {ingredients.filter(item => item.type === 'bun').map(item => <IngredientsCard key={item._id} {...item} />)}
+            <div ref={refBlock} className={`${styles.wrapper_indredients} mt-10 scroll`} onScroll={handleScroll}>
+                <div ref={refTitle1} id="one">
+                    <h2 className={styles.title} >Булки</h2>
+                    <div className={`${styles.ingredient_list} mb-10`}>
+                        {ingredients.filter(item => item.type === 'bun').map(item => <IngredientsCard key={item._id} {...item} />)}
+                    </div>
                 </div>
-                <h2 className={styles.title}>Соусы</h2>
-                <div className={`${styles.ingredient_list} mb-10`}>
-                    {ingredients.filter(item => item.type === 'sauce').map(item => <IngredientsCard key={item._id} {...item} />)}
+                <div ref={refTitle2} id="two">
+                    <h2 className={styles.title} >Соусы</h2>
+                    <div className={`${styles.ingredient_list} mb-10`}>
+                        {ingredients.filter(item => item.type === 'sauce').map(item => <IngredientsCard key={item._id} {...item} />)}
+                    </div>
                 </div>
-                <h2 className={styles.title}>Начинки</h2>
-                <div className={`${styles.ingredient_list} mb-10`}>
-                    {ingredients.filter(item => item.type === 'main').map(item => <IngredientsCard key={item._id} {...item} />)}
+                <div ref={refTitle3} id="three">
+                    <h2 className={styles.title} >Начинки</h2>
+                    <div className={`${styles.ingredient_list} mb-10`}>
+                        {ingredients.filter(item => item.type === 'main').map(item => <IngredientsCard key={item._id} {...item} />)}
+                    </div>
                 </div>
             </div>
         </div>
