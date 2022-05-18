4d7963a3aa5be5af9fc2b7cb33213f14fbead14f
Deryabin Roman <46558697+DeryabinRS@users.noreply.github.com>
2022-05-18 17:09:18 +0700

    add orderCard

---
+++ src/components/order-card/order-card.tsx
@@ -0,0 +1,9 @@
+import styles from './order-card.module.css'
+
+const OrderCard = () => {
+  return (
+    <div className={`${styles.order_card} p-6 mb-6`}>OrderCard</div>
+  )
+}
+
+export default OrderCard
--- src/pages/profile-page/profile-page-orders.tsx
+++ src/pages/profile-page/profile-page-orders.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { FC } from "react";
+import OrderCard from "../../components/order-card/order-card";
 
-const ProfilePageOrders = () => {
-  return (
-    <div>ProfilePageOrders</div>
-  )
-}
+const orders = [
+  {
+    id: 1,
+    number: "034535",
+    name: "Альфа-сахаридный экзо-плантаго флюоресцентный фалленианский бургер",
+    ingredients: [
+      "60d3b41abdacab0026a733c6",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d2",
+      "60d3b41abdacab0026a733d3",
+    ],
+  },
+  {
+    id: 2,
+    number: "034231",
+    name: "Альфа-сахаридный экзо-плантаго флюоресцентный фалленианский бургер",
+    ingredients: [
+      "60d3b41abdacab0026a733c6",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d2",
+      "60d3b41abdacab0026a733d3",
+    ],
+  },
+  {
+    id: 3,
+    number: "034432",
+    name: "Альфа-сахаридный экзо-плантаго флюоресцентный фалленианский бургер",
+    ingredients: [
+      "60d3b41abdacab0026a733c6",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d0",
+      "60d3b41abdacab0026a733d2",
+      "60d3b41abdacab0026a733d3",
+    ],
+  },
+];
 
-export default ProfilePageOrders
+const ProfilePageOrders:FC = () => {
+  	return <div className="scroll pr-2">
+		  {orders.map(order => (
+			  <OrderCard key={order.id} />
+		  ))}
+	</div>;
+};
+
+export default ProfilePageOrders;
